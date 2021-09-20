f256d88d6bd4ceccbc5320f1896df9aa71c22c04
rcarv-rafael <rcarv.rafael@gmail.com>
2021-09-20 01:47:58 -0300

    Execução inicial, sem POM, sem estrutura correta de stepDefs e hooks

---
+++ src/test/java/StepDefinitions/insuranceFormSteps.java
@@ -0,0 +1,43 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class insuranceFormSteps {
+    @Given("user is on main Request Insurance form page")
+    public void user_is_on_main_request_insurance_form_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("to fill out Vehicle data")
+    public void to_fill_out_vehicle_data() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("to fill out Insurant data")
+    public void to_fill_out_insurant_data() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("to fill out Product data")
+    public void to_fill_out_product_data() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("choose Price option")
+    public void choose_price_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("send Quote")
+    public void send_quote() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("see success message.")
+    public void see_success_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
